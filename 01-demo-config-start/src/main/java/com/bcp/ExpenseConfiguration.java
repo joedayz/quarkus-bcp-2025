7da9723cc1135195964b2b3b5c88a584f5b7217a
Jose Amadeo Diaz <jamdiazdiaz@gmail.com>
2025-07-18 12:53:28 -0400

    step 2

---
+++ 01-demo-config-start/src/main/java/com/bcp/ExpenseConfiguration.java
@@ -0,0 +1,19 @@
+package com.bcp;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+
+import java.util.Optional;
+
+@ConfigMapping(prefix = "expense")
+public interface ExpenseConfiguration {
+
+    @WithDefault( "false")
+    boolean debugEnabled();
+
+    Optional<String> debugMessage();
+
+    int rangeHigh();
+
+    int rangeLow();
+}
