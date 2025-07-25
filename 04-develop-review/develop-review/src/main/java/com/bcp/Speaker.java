bb1d0ff7f2aa0ea9f95f36a04350efa32e7726d0
Jose Amadeo Diaz <jamdiazdiaz@gmail.com>
2025-07-25 12:13:42 -0500

    develop review solution

--- 04-develop-review/develop-review/src/main/java/com/bcp/Speaker.java
+++ 04-develop-review/develop-review/src/main/java/com/bcp/Speaker.java
@@ -1,9 +1,17 @@
 package com.bcp;
 
-import java.util.UUID;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 
-public class Speaker {
-    public String id = UUID.randomUUID().toString();
+import java.util.List;
+
+@Entity
+public class Speaker extends PanacheEntity {
     public String name;
     public String organization;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    public List<Talk> talks;
 }
--- 04-develop-review/develop-review/src/main/java/com/bcp/SpeakerResource.java
+++ 04-develop-review/develop-review/src/main/java/com/bcp/SpeakerResource.java
@@ -1,7 +1,14 @@
 package com.bcp;
 
+import io.quarkus.panache.common.Sort;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
 import java.net.URI;
 import java.util.List;
 import java.util.ArrayList;
@@ -11,21 +18,56 @@ import java.util.ArrayList;
 @Produces(MediaType.APPLICATION_JSON)
 public class SpeakerResource {
 
-    List<Speaker> speakers = new ArrayList<>();
-
     @GET
-    public List<Speaker> getSpeakers() {
-        return speakers;
+    @Operation(summary = "Retrieves the list of speakers")
+    @APIResponse(responseCode = "200")
+    public List<Speaker> getSpeakers(
+                                     @DefaultValue("id") @QueryParam("sortBy") String sortBy,
+                                     @DefaultValue("0") @QueryParam("pageIndex") int pageIndex,
+                                     @DefaultValue("25") @QueryParam("pageSize") int pageSize
+    ) {
+        return Speaker
+                .findAll(Sort.by(filterSortBy(sortBy)))
+                .page(pageIndex, pageSize)
+                .list();
     }
 
     @POST
+    @Transactional
+    @Operation(summary = "Adds a speaker")
+    @APIResponse(
+            responseCode = "201",
+            headers = {
+                    @Header(
+                            name = "id",
+                            description = "ID of the created entity",
+                            schema = @Schema(implementation = Integer.class)
+                    ),
+                    @Header(
+                            name = "location",
+                            description = "URI of the created entity",
+                            schema = @Schema(implementation = String.class)
+                    ),
+            },
+            description = "Entity successfully created"
+    )
     public Response createSpeaker(Speaker newSpeaker, @Context UriInfo uriInfo) {
-        speakers.add(newSpeaker);
+        newSpeaker.persist();
 
         return Response.created(generateUriForSpeaker(newSpeaker, uriInfo))
             .header("id", newSpeaker.id)
             .build();
     }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public void deleteSpeaker(@PathParam("id") Long id) {
+        if(!Speaker.deleteById(id)){
+            throw new NotFoundException();
+        }
+    }
+
 
     private URI generateUriForSpeaker(Speaker speaker, UriInfo uriInfo) {
         return uriInfo.getAbsolutePathBuilder().path("/{id}").build(speaker.id);
