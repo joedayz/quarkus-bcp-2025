d956fc2b821892d52f82a3957cd39c75ae50c391
Jose Diaz <jose.diaz@joedayz.pe>
2025-08-04 12:42:20 -0500

    develop solution

--- 05-reactive-develop/suggestions/src/main/java/com/bcp/SuggestionResource.java
+++ 05-reactive-develop/suggestions/src/main/java/com/bcp/SuggestionResource.java
@@ -1,24 +1,26 @@
 package com.bcp;
 
-import java.util.List;
-
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-
-import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
-import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/suggestion")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class SuggestionResource {
+
+    @POST
+    @WithTransaction
+    public Uni<Suggestion> create(Suggestion newSuggestion) {
+        return newSuggestion.persist();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Uni<Suggestion> get(Long id){
+        return Suggestion.findById(id);
+    }
 
     @DELETE
     public Uni<Long> deleteAll() {
