f8b05cb2dbd669b20d357b6d26fe76f719c2f1f7
Jose Diaz <jose.diaz@joedayz.pe>
2025-08-07 12:26:46 -0500

    pagina web

--- 06-eda-start/fraud-detector/src/main/java/com/bcp/reactive/FraudProcessor.java
+++ 06-eda-start/fraud-detector/src/main/java/com/bcp/reactive/FraudProcessor.java
@@ -2,15 +2,50 @@ package com.bcp.reactive;
 
 
 import com.bcp.event.BankAccountWasCreated;
+import com.bcp.event.HighRiskAccountWasDetected;
+import com.bcp.event.LowRiskAccountWasDetected;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.jboss.logging.Logger;
+
+import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class FraudProcessor {
     private static final Logger LOGGER = Logger.getLogger(FraudProcessor.class);
 
+    @Channel("low-risk-alerts-out")
+    Emitter<LowRiskAccountWasDetected> lowRiskEmitter;
 
+    @Channel("high-risk-alerts-out")
+    Emitter<HighRiskAccountWasDetected> highRiskEmitter;
 
+    @Incoming("new-bank-accounts-in")
+    public CompletionStage<Void> sendEventNotifications(Message<BankAccountWasCreated> message){
+        BankAccountWasCreated event = message.getPayload();
+
+        logBankAccountWasCreatedEvent(event);
+
+        Integer fraudScore = calculateFraudScore(event.balance);
+
+        logFraudScore(event.id, fraudScore);
+
+        if(fraudScore>50){
+            logEmitEvent("HighRiskAccountWasDetected", event.id);
+            highRiskEmitter.send(
+                    new HighRiskAccountWasDetected(event.id)
+            );
+        }else if(fraudScore>20){
+            logEmitEvent("LowRiskAccountWasDetected", event.id);
+            lowRiskEmitter.send(
+                    new LowRiskAccountWasDetected(event.id)
+            );
+        }
+        return message.ack();
+    }
 
     private Integer calculateFraudScore(Long amount) {
         if (amount > 25000) {
