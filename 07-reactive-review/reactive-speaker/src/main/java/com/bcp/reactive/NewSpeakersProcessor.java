b402dec61b6ff5e1ce6ecbf9bbcf882b15d42422
Jose Diaz <jose.diaz@joedayz.pe>
2025-08-11 11:31:35 -0500

    NewSpeakersProcessor ready

--- 07-reactive-review/reactive-speaker/src/main/java/com/bcp/reactive/NewSpeakersProcessor.java
+++ 07-reactive-review/reactive-speaker/src/main/java/com/bcp/reactive/NewSpeakersProcessor.java
@@ -1,12 +1,47 @@
 package com.bcp.reactive;
 
+import com.bcp.event.EmployeeSignedUp;
+import com.bcp.event.SpeakerWasCreated;
+import com.bcp.event.UpstreamMemberSignedUp;
 import com.bcp.model.Affiliation;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.jboss.logging.Logger;
+
+import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class NewSpeakersProcessor {
     private static final Logger LOGGER = Logger.getLogger(NewSpeakersProcessor.class);
+
+    @Channel("employees-out")
+    Emitter<EmployeeSignedUp> employeeEmitter;
+
+    @Channel("upstream-members-out")
+    Emitter<UpstreamMemberSignedUp> upstreamEmitter;
+
+    @Incoming("new-speakers-in")
+    public CompletionStage<Void> sendEventNotifications(Message<SpeakerWasCreated> message) {
+
+        SpeakerWasCreated event = message.getPayload();
+
+        logProcessEvent(event.id);
+
+        if(event.affiliation == Affiliation.RED_HAT){
+            logEmitEvent("EmployeesSignedUp", event.affiliation);
+            employeeEmitter.send(new EmployeeSignedUp(event.id, event.fullName,  event.email));
+
+        }else if(event.affiliation == Affiliation.GNOME_FOUNDATION) {
+            logEmitEvent("UpstreamMemberSignedUp", event.affiliation);
+            upstreamEmitter.send(new UpstreamMemberSignedUp(event.id, event.fullName, event.email));
+        }
+
+        return message.ack();
+    }
+
 
     private void logEmitEvent(String eventName, Affiliation affiliation) {
         LOGGER.infov(
